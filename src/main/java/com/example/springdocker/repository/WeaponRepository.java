b818f91eaf79006ca0da6d58b00d2511566a043a
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 20:11:34 +0200

    Added @Repository to WeaponRepository.java

--- src/main/java/com/example/springdocker/repository/WeaponRepository.java
+++ src/main/java/com/example/springdocker/repository/WeaponRepository.java
@@ -4,8 +4,11 @@ package com.example.springdocker.repository;
 import com.example.springdocker.model.Weapon;
 import com.example.springdocker.model.WeaponType;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface WeaponRepository extends MongoRepository<Weapon , String> {
     List<Weapon> FindWeaponByType(WeaponType type);
     List<Weapon> FindWeaponByAttackValue(double attackValue);
