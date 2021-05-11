13c1560c5d4f4745deb9515e310f3e50e4fe1c2d
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 21:31:28 +0200

    Created WeaponController.java

---
+++ src/main/java/com/example/springdocker/controller/WeaponController.java
@@ -0,0 +1,36 @@
+package com.example.springdocker.controller;
+
+import com.example.springdocker.model.WeaponType;
+import com.example.springdocker.service.WeaponService;
+import com.example.springdocker.model.Weapon;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class WeaponController {
+    private final WeaponService service;
+
+    @GetMapping("/Weapon")
+    public List<Weapon> getWeapon(){
+        return service.getWeapon();
+    }
+
+    @PostMapping("/Weapon")
+    public void saveNewWeapon(@RequestBody Weapon weapon) {service.saveNewWeapon(weapon);}
+
+    @GetMapping("/Weapon")
+    public Weapon getWeaponByName(@RequestBody String name) {return service.getWeaponByName(name);}
+
+    @GetMapping("/Weapon")
+    public Weapon getWeaponByType(@RequestBody WeaponType type) {return service.getWeaponByType(type);}
+
+    @GetMapping("/Weapon")
+    public Weapon getWeaponByAttackValue(@RequestBody double attackValue) {return service.getWeaponByAttackValue(attackValue);}
+
+}
