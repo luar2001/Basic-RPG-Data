e697e6bd7df185e9e0d5477258e4f21f1bc4a02c
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 19:32:12 +0200

    Created species.java

---
+++ src/main/java/com/example/springdocker/model/Species.java
@@ -0,0 +1,15 @@
+package com.example.springdocker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * object that represent the fantasy Species like for example Dwarf or Elf
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Species {
+    String Species;
+}
