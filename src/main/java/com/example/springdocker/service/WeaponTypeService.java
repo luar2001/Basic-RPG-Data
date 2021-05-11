a655606e2951a1cbc80e1898c6e9ac5a4227e9b5
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 21:25:14 +0200

    Created WeaponTypeService.java

---
+++ src/main/java/com/example/springdocker/service/WeaponTypeService.java
@@ -0,0 +1,27 @@
+package com.example.springdocker.service;
+
+import com.example.springdocker.model.WeaponType;
+import com.example.springdocker.repository.WeaponTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class WeaponTypeService {
+
+    private final WeaponTypeRepository repository;
+
+    public List<WeaponType> getWeaponTypes() {
+        return repository.findAll();
+    }
+
+    public WeaponType getWeaponTypesByType(String type) {
+        return repository.FindWeaponTypeByType(type);
+    }
+
+    public void saveNewWeaponType(WeaponType weaponType) {
+        repository.save(weaponType);
+    }
+}
