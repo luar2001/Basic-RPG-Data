36baa3c39509a5d0ff254c18d41f07e5c084873e
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 19:18:28 +0200

    Created WeaponType.java

--- src/main/java/com/example/springdocker/model/WeaponType.java
+++ src/main/java/com/example/springdocker/model/WeaponType.java
@@ -1,8 +1,15 @@
-package com.example.springdocker.model;/**
-Created by Name Here
-Date: 11/05/2021
-Time: 19:15
-Project: spring-docker-demo
-Copyright: MIT
-**/public class WeaponType {
+package com.example.springdocker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *Object that represent the type of weapon for example a sword
+ **/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WeaponType {
+    String type;
 }
