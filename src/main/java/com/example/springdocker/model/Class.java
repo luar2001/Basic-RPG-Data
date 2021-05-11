94252c48eecf2b564838cee3d3aedbddd66579f6
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 19:13:45 +0200

    created Class.java

---
+++ src/main/java/com/example/springdocker/model/Class.java
@@ -0,0 +1,16 @@
+package com.example.springdocker.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * object that represent an rpg class for example mage
+ *
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Class {
+    private String className;
+}
