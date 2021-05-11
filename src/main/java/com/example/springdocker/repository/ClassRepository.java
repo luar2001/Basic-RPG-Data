129d658d40d7433cd0681b4d5eea6c1bdf26f755
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 20:03:17 +0200

    Created ClassRepository.java

---
+++ src/main/java/com/example/springdocker/repository/ClassRepository.java
@@ -0,0 +1,10 @@
+package com.example.springdocker.repository;
+
+import com.example.springdocker.model.Class;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClassRepository extends MongoRepository<Class, String> {
+    String FindClassByClassName(String className);
+}
