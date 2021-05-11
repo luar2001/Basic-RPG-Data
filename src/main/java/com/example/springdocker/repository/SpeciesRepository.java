74d64f758e75a369e1b62b71e4256448691bded7
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-11 20:12:41 +0200

    Created SpeciesRepository.java

---
+++ src/main/java/com/example/springdocker/repository/SpeciesRepository.java
@@ -0,0 +1,10 @@
+package com.example.springdocker.repository;
+
+import com.example.springdocker.model.Species;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SpeciesRepository extends MongoRepository<Species, String> {
+    Species FindSpeciesByName(String name);
+}
