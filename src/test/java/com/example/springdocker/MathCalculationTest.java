d8a2396ba9586fdc9f9447c332c2492e85223d57
Luar2001 <Lukas.Aronsson@protonmail.com>
2021-05-14 21:49:23 +0200

    Created tests for all methods in MathCalculationTest

---
+++ src/test/java/com/example/springdocker/MathCalculationTest.java
@@ -0,0 +1,72 @@
+package com.example.springdocker;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Created by Name Here
+ * Date: 14/05/2021
+ * Time: 21:21
+ * Project: spring-docker-demo
+ * Copyright: MIT
+ **/
+class MathCalculationTest {
+
+    MathCalculation math;
+
+    @BeforeEach
+    void setUp() {
+        math = new MathCalculation();
+    }
+
+    @DisplayName("MathCalculation Addition test")
+    @Test
+    void add() {
+        assertEquals(4, math.add(2, 2));
+        assertEquals(1, math.add(1, 0));
+        assertEquals(0, math.add(-1, 1));
+    }
+
+    @DisplayName("MathCalculation Multiplication test")
+    @Test
+    void multiply() {
+        assertEquals(4, math.multiply(2, 2));
+        assertEquals(0, math.multiply(1, 0));
+        assertEquals(1, math.multiply(1, 1));
+        assertEquals(-1, math.multiply(-1, 1));
+    }
+
+    @DisplayName("MathCalculation division test")
+    @Test
+    void divide() {
+        assertEquals(1, math.divide(2, 2));
+        assertEquals(2, math.divide(4, 2));
+        assertEquals(2.5, math.divide(5, 2));
+        assertEquals(-1, math.divide(-1, 1));
+    }
+
+    @DisplayName("MathCalculation division throw when parameter 0 test")
+    @Test
+    void divideThrow() {
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    math.divide(2, 0);
+                });
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    math.divide(0, 0);
+                });
+
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    math.divide(2, 0);
+                });
+
+    }
+}
